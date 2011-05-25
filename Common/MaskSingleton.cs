1f3d11b61e303fd6bae96bfdeb0f041a60f34c04
eanhernandez@eanh.net <eanhernandez@eanh.net@8887ccf0-97a5-d2cb-873f-37d9dbf988da>
2011-05-25 04:02:37 +0000

    Now the observer is working in CSVEater, OME and Ticker.  Also, a new singleton controls the logic for whether an Observer should print/email/log it's data.

---
+++ Common/MaskSingleton.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace Common
+{
+    public sealed class MaskSingleton
+    {
+        static MaskSingleton _instance = null;
+        static string RtmLevel = "" ;
+        static readonly object Padlock = new object();
+        public bool ShouldThisObserverTakeAction(LoggerObserver l)
+        {
+            switch (RtmLevel)
+            {
+                case "DEBUG":
+                    return true;
+                case "VERBOSE":
+                    return true;
+                case "NORMAL":
+                    return true;
+                default:
+                    return false;
+            }
+        }
+        public bool ShouldThisObserverTakeAction(EmailerObserver e)
+        {
+            switch (RtmLevel)
+            {
+                case "DEBUG":
+                    return false;
+                case "VERBOSE":
+                    return true;
+                case "NORMAL":
+                    return false;
+                default:
+                    return false;
+            }
+ 
+        }
+        public bool ShouldThisObserverTakeAction(ScreenPrinterObserver s)
+        {
+            switch (RtmLevel)
+            {
+                case "DEBUG":
+                    return true;
+                case "VERBOSE":
+                    return true;
+                case "NORMAL":
+                    return false;
+                default:
+                    return false;
+            }
+        }
+        MaskSingleton(){}
+        public static MaskSingleton Instance
+        {
+            get
+            {
+                lock (Padlock)
+                {
+                    if (_instance == null)
+                    {
+                        RtmLevel = Convert.ToString(ConfigurationManager.AppSettings["RTMLevel"]);
+                        _instance = new MaskSingleton();
+                    }
+                    return _instance;
+                }
+            }
+        }
+    }
+}
